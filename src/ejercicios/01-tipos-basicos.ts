b169b6e720dd785ea9919563a1b5a599c903934e
Martin <brissetmartin.com>
2021-02-04 17:31:33 -0300

    Inicio ts

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+
+let nombre: string = 'Strideddr'
+let hp: number | string = 95
+let estaVivo: boolean = true
+
+hp = 'full'
+
+console.log(nombre, hp);
